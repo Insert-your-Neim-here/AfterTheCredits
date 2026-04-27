6ce8dad66fe1158c7499d4c8337f482b13d76e1f
Insert-your-Neim-here <neimedas.tamasauskas@gmail.com>
2026-04-27 19:37:42 +0100

    +

--- backend/movies/urls.py
+++ backend/movies/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'movies'
 urlpatterns = [
   path('browse/', views.browse_view, name='browse'),
-  path('wishlist/', views.wishlist_view, name='wishlist'),
-  path('wishlist/toggle/<int:tmdb_id>/', views.toggle_wishlist_view, name='toggle_wishlist'),
   path('details/<int:tmdb_id>/', views.movie_detail_view, name='details'),
+  path("<int:tmdb_id>/wishlist/toggle/", views.toggle_wishlist_view, name="toggle_wishlist"),
+  path("wishlist/", views.wishlist_view, name="wishlist"),
 ]
